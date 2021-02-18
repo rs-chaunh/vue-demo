d27cdfdc97c85625ed9c08a1dcf6bbb7713db9b8
nhuquynh1499 <ngngocnhuquynh0104@gmail.com>
2021-02-18 14:38:24 +0700

    Update file App.vue and main.js

--- rs-quynh/src/main.js
+++ rs-quynh/src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./stores";
 
 createApp(App)
+  .use(router)
+  .use(store)
   .mount("#app");
