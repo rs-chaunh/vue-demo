e2aac13b607685638bc21110832c0af0d9d5b111
nhuquynh1499 <ngngocnhuquynh0104@gmail.com>
2021-03-01 08:58:15 +0700

    Navigation Guards

--- rs-quynh/src/router.js
+++ rs-quynh/src/router.js
@@ -1,5 +1,18 @@
 import { createWebHistory, createRouter } from "vue-router";
-import store from "./stores"
+import store from "./stores";
+
+function authenticated(to, from, next) {
+  if (to.path === "/auth") {
+    if (store.getters.isAuthenticated) {
+      router.push({ name: "Coaches" });
+    } else next();
+  } else {
+    if (!store.getters.isAuthenticated) {
+      router.push({ name: "Auth" });
+    } else next();
+  }
+}
+
 const routes = [
   {
     path: "/",
@@ -26,31 +39,19 @@ const routes = [
     path: "/auth",
     name: "Auth",
     component: () => import("./components/AuthPage.vue"),
-    beforeEnter: (to, from, next) => {
-      if (store.getters.isAuthenticated) {
-        router.push({ name: "Coaches" });
-      } else next();
-    },
+    beforeEnter: authenticated,
   },
   {
     path: "/register",
     name: "Register",
     component: () => import("./components/RegisterPage.vue"),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters.isAuthenticated) {
-        router.push({ name: "Auth" });
-      } else next();
-    },
+    beforeEnter: authenticated,
   },
   {
     path: "/requests",
     name: "Requests",
     component: () => import("./components/RequestsPage.vue"),
-    beforeEnter: (to, from, next) => {
-      if (!store.getters.isAuthenticated) {
-        router.push({ name: "Auth" });
-      } else next();
-    },
+    beforeEnter: authenticated,
   },
   {
     path: "/:pathMatch(.*)*",
@@ -63,6 +64,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
 
 export default router;
